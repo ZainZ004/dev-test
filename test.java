50762149e0289179dba672e40132010aa726eb3c
ziazhang <ziazhang2004@gmail.com>
2018-08-06 17:13:15 +0800

    java IO study

--- test.java
+++ test.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
 public class test{
     public static void main(String[] args) {
-        System.out.print("This is a test.");   
+        System.out.print("This is a test.");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Pleasr enter your name");
+        String name = scanner.next();
+        System.out.printf("Hello %s\n",name);
     }
 }
